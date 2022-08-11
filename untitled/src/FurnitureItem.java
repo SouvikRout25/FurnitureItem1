56400405d986a4c8383ca00da4ae8613abfdfada
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-08-11 13:09:22 +0530

    initialze again

--- untitled/src/FurnitureItem.java
+++ untitled/src/FurnitureItem.java
@@ -5,4 +5,14 @@ public class FurnitureItem {
     String colour;
     String usage;
     double price;
+    FurnitureItem(){
+        furnitureCode = 0;
+        Types = "Table";
+        grade = "Grade 1";
+        colour = "red";
+        usage = "Indoor";
+        price = 0.0;
+    }
+
+
 }
